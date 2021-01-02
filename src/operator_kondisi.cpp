fae320b676e057740a8c7dd05908ad6e8f1d1a8b
hadi499 <hadirpm9@gmail.com>
2021-01-02 23:47:54 +0700

    update

---
+++ src/operator_kondisi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b, c;
+    a = 5;
+    b= 7;
+    c = ( a > b ) ? a : b;
+    cout << c << endl;
+    cin.get();
+    return 0;
+}
